e6722151dd8527255ef796ba62c70dab5655e0ac
Muazzam Gilani <muazzam.gilani@kellogg.northwestern.edu>
2024-01-17 18:14:15 +0000

    MG - completed airbnb practice

--- practice-6.sql
+++ practice-6.sql
@@ -1,4 +1,8 @@
 -- How many reviews are written per neighborhood?
+SELECT listings.neighborhood, COUNT (reviews.comments)
+FROM reviews
+INNER JOIN listings ON listings.id = reviews.listing_id
+GROUP BY listings.neighborhood;
 
 -- +------------------------+----------+
 -- | Albany Park            | 2557     |
